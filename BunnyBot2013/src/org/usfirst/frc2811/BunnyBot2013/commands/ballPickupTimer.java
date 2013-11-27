0e9e79d4431bce2ec43924bbcab4112d920fad44
austindb98 <austindbryant@gmail.com>
2013-11-26 20:02:05 -0800

    Initial Commit
    Started structure.

---
+++ BunnyBot2013/src/org/usfirst/frc2811/BunnyBot2013/commands/ballPickupTimer.java
@@ -0,0 +1,11 @@
+
+package org.usfirst.frc2811.BunnyBot2013.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class ballPickupTimer extends CommandGroup {
+    
+    public ballPickupTimer() {
+        
+    }
+}
