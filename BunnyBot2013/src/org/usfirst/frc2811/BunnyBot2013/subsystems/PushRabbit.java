dd70154ae4f9ca0ab17a9717f3ff5faf30e5f1ed
austindb98 <austindbryant@gmail.com>
2013-12-04 20:06:53 -0800

    Re-Re-Commit
    Made PushRabbit a subsystem.

---
+++ BunnyBot2013/src/org/usfirst/frc2811/BunnyBot2013/subsystems/PushRabbit.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc2811.BunnyBot2013.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author 128925
+ */
+public class PushRabbit extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    public void pushRabbit(){
+        //if(piston.get()==false){
+           //piston.set()=true;
+        //}         
+    }
+}
