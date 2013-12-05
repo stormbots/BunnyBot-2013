262edd6452b2a96997e5bdc94cecf02d53d2dad3
austindb98 <austindbryant@gmail.com>
2013-12-04 19:51:20 -0800

    Re-Initial Commit
    Removed DumpRabbit from Castle Anthrax
    Added PushRabbit w/ more Penguins.

--- BunnyBot2013/src/org/usfirst/frc2811/BunnyBot2013/commands/PushRabbit.java
+++ BunnyBot2013/src/org/usfirst/frc2811/BunnyBot2013/commands/PushRabbit.java
@@ -1,28 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author 128925
- */
-public class DumpRabbit extends CommandBase {
+package org.usfirst.frc2811.BunnyBot2013.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+
+public class PushRabbit extends Command {
     
-    public DumpRabbit() {
+    public PushRabbit() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        //check state, set 0
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        //check state, set 1
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -32,7 +26,6 @@ public class DumpRabbit extends CommandBase {
 
     // Called once after isFinished returns true
     protected void end() {
-        //set piston 0
     }
 
     // Called when another command which requires one or more of the same
